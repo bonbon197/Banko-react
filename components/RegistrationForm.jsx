7ee00a41df97770410e37722774966ed8d617d4c
bonbon <nepunepubon@gmail.com>
2023-09-09 12:50:10 +0800

    Added Bulm and FontAwesome

--- components/RegistrationForm.jsx
+++ components/RegistrationForm.jsx
@@ -1,12 +1,37 @@
+import { Button } from 'react-bulma-components'
+
 const RegistrationForm = () => {
 
     return(
         <>
-            <form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Log in</button>
-            </form>
+        <div className="field">
+            <div className="control has-icons-left has-icons-right">
+                <input className="input" type="text" placeholder="Email Address" />
+                <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                </span>
+                <span className="icon is-small is-right">
+                    <i className="fas fa-check"></i>
+                </span>
+
+            </div>
+        </div>
+
+        <div className="field">
+            <div className="control has-icons-left has-icons-right">
+                <input className="input" type="password" placeholder="Password" />
+                <span className="icon is-small is-left">
+                    <i className="fas fa-lock"></i>
+                </span>
+
+                <span className="icon is-small is-right">
+                    <i className="fas fa-eye"></i>
+                </span>
+
+            </div>
+        </div>
+
+        <Button color="primary">Log In</Button>
         </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,21 @@ function App() {
 
   return (
     <>
-      <div>
+    <section className="section is-large">
+      <div className="card">
+      <div className="logo-container">
 
         <a href="#" target="_blank">
           <img src={bankoLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Banko</h1>
-      <h2>Some Tagline relating sa upuan</h2>
-      <div className="formContainer">
+      <h1 className="title">Banko</h1>
+      <h2 className="subtitle">Some Tagline relating sa upuan</h2>
+      <div className="container">
         <RegistrationForm />
       </div>
-      <div className="card">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </section>
     </>
   )
 }
