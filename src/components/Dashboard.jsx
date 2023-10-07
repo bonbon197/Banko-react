5da0a1359d62babd002d370f253815b8c2930b1a
bonbon <nepunepubon@gmail.com>
2023-10-07 10:55:14 +0800

    what have I done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,168 +1,170 @@
 import { useState, useEffect } from "react";
 import Modal from 'react-modal';
-// import 'react-modal/style.css'
+import { saveToLocalStorage, getFromLocalStorage } from "../db/dbInterface";
+import { DepositModal } from "./modals/DepositModal";
 
+const Dashboard = ({ account }) => {
+  const [modalIsOpen, setModalIsOpen] = useState({
+    deposit: false,
+    withdraw: false,
+    transfer: false,
+  });
 
+  const [amounts, setAmounts] = useState({
+    deposit: 0,
+    withdraw: 0,
+    transfer: 0,
+  });
 
-const Dashboard = ({account}) => {
+  const [errors, setErrors] = useState({
+    deposit: '',
+    withdraw: '',
+    transfer: '',
+  });
 
-  const [depositModalIsOpen, setDepositModalIsOpen] = useState(false);
-  const [withdrawModalIsOpen, setWithdrawModalIsOpen] = useState(false);
-  const [transferModalIsOpen, setTransferModalIsOpen] = useState(false);
+  const [currentBalance, setCurrentBalance] = useState(0);
 
-  const [depositAmount, setDepositAmount] = useState(0);
-  const [withdrawAmount, setWithdrawAmount] = useState(0);
-  const [transferAmount, setTransferAmount] = useState(0);
-  const [transferAccount, setTransferAccount] = useState('');
+  useEffect(() => {
+    if (account) {
+      setCurrentBalance(account.balance);
+    }
+  }, [account]);
 
-  const [depositError, setDepositError] = useState('');
-  const [withdrawError, setWithdrawError] = useState('');
-  const [transferError, setTransferError] = useState('');
+  const openModal = (type) => {
+    setModalIsOpen({ ...modalIsOpen, [type]: true });
+  }
 
+  const closeModal = (type) => {
+    setModalIsOpen({ ...modalIsOpen, [type]: false });
+  }
+
+  const handleChangeAmount = (type, e) => {
+    setAmounts({ ...amounts, [type]: e.target.value });
+    setErrors({ ...errors, [type]: '' });
+  }
 
   const handleDeposit = (e) => {
     e.preventDefault();
+    const depositAmount = parseInt(amounts.deposit);
+    
     if (depositAmount <= 0) {
-      setDepositError('Please enter a valid amount');
+      setErrors({ ...errors, deposit: 'Please enter a valid amount' });
       return;
     }
+    
+    const newBalance = account.balance + depositAmount;
+    const updatedAccount = { ...account, balance: newBalance };
 
-    const newBalance = account.balance + parseInt(depositAmount);
-    const updatedAccount = {...account, balance: newBalance};
+    const accounts = getFromLocalStorage('accounts');
 
-    setDepositModalIsOpen(false);
-    setDepositError('');
+    const updatedAccounts = accounts.map((acc) =>
+      acc.id === updatedAccount.id ? updatedAccount : acc
+    );
+
+    saveToLocalStorage(updatedAccounts);
+    setCurrentBalance(newBalance);
+    closeModal('deposit');
   }
 
   const handleWithdraw = (e) => {
     e.preventDefault();
+    const withdrawAmount = parseInt(amounts.withdraw);
+
     if (withdrawAmount <= 0) {
-      setWithdrawError('Please enter a valid amount');
+      setErrors({ ...errors, withdraw: 'Please enter a valid amount' });
       return;
     } else if (withdrawAmount > account.balance) {
-      setWithdrawError('You do not have enough funds');
+      setErrors({ ...errors, withdraw: 'You do not have enough funds' });
       return;
     }
 
-    setWithdrawModalIsOpen(false);
-    setWithdrawError('');
+    closeModal('withdraw');
   }
 
   const handleTransfer = (e) => {
     e.preventDefault();
+    const transferAmount = parseInt(amounts.transfer);
+
     if (transferAmount <= 0) {
-      setTransferError('Please enter a valid amount');
+      setErrors({ ...errors, transfer: 'Please enter a valid amount' });
       return;
     } else if (transferAmount > account.balance) {
-      setTransferError('You do not have enough funds');
+      setErrors({ ...errors, transfer: 'You do not have enough funds' });
       return;
     }
 
-    setTransferModalIsOpen(false);
-    setTransferError('');
+    closeModal('transfer');
   }
 
   useEffect(() => {
     if (account) {
-      setDepositAmount(0);
-      setWithdrawAmount(0);
-      setTransferAmount(0);
-      setTransferAccount('');
+      setAmounts({
+        deposit: 0,
+        withdraw: 0,
+        transfer: 0,
+      });
+      setErrors({
+        deposit: '',
+        withdraw: '',
+        transfer: '',
+      });
+      setCurrentBalance(account.balance);
     }
   }, [account]);
 
-
-  const openDepositModal = () => {
-    console.log('open deposit modal')
-    setDepositModalIsOpen(true);
-  }
-  const closeDepositModal = () => {
-    console.log('close deposit modal')
-    setDepositModalIsOpen(false);
-  }
-
-  const openWithdrawModal = () => {
-    console.log('open withdraw modal')
-    setWithdrawModalIsOpen(true);
-  }
-  const closeWithdrawModal = () => {
-    console.log('close withdraw modal')
-    setWithdrawModalIsOpen(false);
-  }
-
-  const openTransferModal = () => {
-    console.log('open transfer modal')
-    setTransferModalIsOpen(true);
-  }
-  const closeTransferModal = () => {
-    console.log('close transfer modal')
-    setTransferModalIsOpen(false);
-  }
-
   if (!account) {
-    return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    return "huh";
   }
 
-
-
-    return(
-      <>
+  return (
+    <>
       <section className="section is-large">
         <div className="card">
-
-        <h2 className="subtitle">Hello {account.firstName}</h2>
-        <h2 className="subtitle">Your current balance is {account.balance}</h2>
-        <div className="container">
-          <h2>wat do</h2>
-          <div className="field is-grouped">
-            <p className="control">
-              <button className="button is-primary" onClick={openDepositModal}>Deposit</button>
-            </p>
-            <p className="control">
-              <button className="button is-link" onClick={openWithdrawModal}>Withdraw</button>
-            </p>
-            <p className="control">
-              <button className="button is-link" onClick={openTransferModal}>Transfer</button>
-            </p>
+          <h2 className="subtitle">Hello {account.firstName}</h2>
+          <h2 className="subtitle">Your current balance is {currentBalance}</h2>
+          <div className="container">
+            <h2>wat do</h2>
+            <div className="field is-grouped">
+              <p className="control">
+                <button className="button is-primary" onClick={() => openModal('deposit')}>Deposit</button>
+              </p>
+              <p className="control">
+                <button className="button is-link" onClick={() => openModal('withdraw')}>Withdraw</button>
+              </p>
+              <p className="control">
+                <button className="button is-link" onClick={() => openModal('transfer')}>Transfer</button>
+              </p>
+            </div>
           </div>
-        </div>
         </div>
       </section>
 
+      <DepositModal
+        isOpen={modalIsOpen.deposit}
+        onClose={() => closeModal('deposit')}
+        onDeposit={handleDeposit}
+        amount={amounts.deposit}
+        onChangeAmount={(e) => handleChangeAmount('deposit', e)}
+        depositError={errors.deposit}
+      />
+
       <Modal
-        isOpen={depositModalIsOpen}
-        onRequestClose={closeDepositModal}
-        contentLabel="Deposit Modal"
+        isOpen={modalIsOpen.withdraw}
+        onRequestClose={() => closeModal('withdraw')}
+        contentLabel="Withdraw Modal"
         ariaHideApp={false}
-        className="modal"
-        overlayClassName="modal-background"
       >
-        <div className="modal">
-          <div className="modal-background"></div>
-          <div className="modal-content">
-            <form onSubmit={handleDeposit}>
-              <div className="field">
-                <label className="label">Deposit Amount</label>
-                <div className="control">
-                  <input className="input" type="number" placeholder="Enter amount" value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} />
-                </div>
-                {depositError && <p className="help is-danger">{depositError}</p>}
-              </div>
-              <div className="field is-grouped">
-                <div className="control">
-                  <button className="button is-link">Deposit</button>
-                </div>
-                <div className="control">
-                  <button className="button is-link is-light" onClick={closeDepositModal}>Cancel</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
       </Modal>
 
-      </>
-    );
+      <Modal
+        isOpen={modalIsOpen.transfer}
+        onRequestClose={() => closeModal('transfer')}
+        contentLabel="Transfer Modal"
+        ariaHideApp={false}
+      >
+      </Modal>
+    </>
+  );
 }
 
 export default Dashboard;
